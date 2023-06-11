6157dc760132d009ff3d44b3c18d904d97ec5d89
gulybyte <gulybyte@gmail.com>
2023-06-11 06:40:02 -0300

    add primeflex to ui

--- todo-ui/src/main.js
+++ todo-ui/src/main.js
@@ -4,11 +4,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config';
+import InputText from 'primevue/inputtext';
+
 
 const app = createApp(App)
 
 app.use(router)
 
 app.use(PrimeVue)
+app.component('InputText', InputText)
 
 app.mount('#app')
