53e04620d64499fd1565b4cdfffc11764e36cd0a
gulybyte <gulybyte@gmail.com>
2023-06-11 08:51:21 -0300

    tabela com dados

--- todo-ui/src/main.js
+++ todo-ui/src/main.js
@@ -3,10 +3,14 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import PrimeVue from 'primevue/config';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
-
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';                   // optional
 
 
 const app = createApp(App)
@@ -16,6 +20,11 @@ app.use(router)
 app.use(PrimeVue)
 app.component('InputText', InputText)
 app.component('Button', Button)
+app.component('DataTable', DataTable)
+app.component('Column', Column)
+app.component('ColumnGroup', ColumnGroup)
+app.component('Row', Row)
+
 
 
 app.mount('#app')
--- todo-ui/src/views/HomeView.vue
+++ todo-ui/src/views/HomeView.vue
@@ -1,28 +1,67 @@
 <template>
+  <div>
+    <InputText v-model="description" type="text" style="width: 100%; margin-bottom: 0.5rem;" />
 
-  <InputText v-model="description" type="text" style="width: 100%; margin-bottom: 0.5rem;" />
+    <Button label="Salvar" @click="postData" :disabled="description === ''"></Button>
 
-    <Button label="Salvar" @click="postData"
-      :disabled="description === ''"></Button>
-
+    <div class="card">
+      <DataTable :value="products" tableStyle="min-width: 50rem">
+        <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header">
+          <template #body>
+            <template v-if="col.field === 'id'">
+              <Button>{{ col.field }}</Button>
+            </template>
+            <template v-else>
+              {{ col.field }}
+            </template>
+          </template>
+        </Column>
+      </DataTable>
+    </div>
+  </div>
 </template>
 
 
 
+
+
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue';
 
-  const URL = 'http://localhost:8080/api-rest/todos/'
-  const HEADERS = { 'Content-Type': 'application/json' }
+const URL = 'http://localhost:8080/api-rest/todos/'
+const HEADERS = { 'Content-Type': 'application/json' }
 
-  const description = ref('')
+const description = ref('')
+const products = ref([])
 
-  async function postData()
-  {
+
+const columns = [
+  { field: 'description', header: 'Descrição' },
+  { field: 'createdDate', header: 'Data Criação' },
+  { field: 'id', header: 'Editar' },
+];
+
+
+
+async function fetchData() {
+  const response = await fetch(URL)
+  if (response.ok) {
+    products.value = await response.json()
+  }
+}
+
+
+
+async function postData() {
+  if (description.value !== '') {
     await fetch(URL, {
       method: 'POST',
       headers: HEADERS,
       body: JSON.stringify({ 'description': description.value })
     })
+    fetchData()
   }
+}
+
+onMounted(fetchData)
 </script>
