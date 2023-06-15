2f4049254245f974085bd60d45e3cbe93a477066
gulybyte <gulybyte@gmail.com>
2023-06-15 13:36:07 -0300

    UI para updateDescriptionById

--- todo-ui/src/views/HomeView.vue
+++ todo-ui/src/views/HomeView.vue
@@ -6,25 +6,34 @@
       class="mb-05rem" style="margin-bottom: 0.5rem;"></Button>
 
     <div class="card">
-      <DataTable :value="todos" tableStyle="min-width: 50rem" stripedRows>
+      <DataTable :value="todos" tableStyle="min-width: 50rem" stripedRows editMode="cell" @cell-edit-complete="onCellEditComplete">
+
         <Column field="createdDate" header="Data" sortable>
           <template #body="createdDate">
             {{ removeYear(createdDate.data.createdDate) }}
           </template>
         </Column>
+
+        <Column field="description" header="Descrição">
+          <template #body="description">
+            {{ description.data.description }}
+          </template>
+          <template #editor="{ data, field }">
+            <InputText v-model="data[field]" autofocus style="width: 100%;" />
+          </template>
+        </Column>
+
         <Column v-for="col of columns" :key="col.field" :field="col.field" :header="col.header">
           <template #body="rowData">
             <template v-if="col.field === 'id'">
               <Button class="mark-done-button" @click="markAsDone(rowData.data.id)"><i class="pi pi-check"></i></Button>
-            </template>
-            <template v-if="col.field === 'description'">
-              {{ rowData.data.description }}
             </template>
             <template v-if="col.field === 'orderTodo'">
               <Button class="change-order-button" @click="changeOrderById(rowData.data.id)"><i class="pi pi-angle-double-up"></i></Button>
             </template>
           </template>
         </Column>
+
       </DataTable>
     </div>
   </div>
@@ -44,17 +53,28 @@ const description = ref('')
 const todos = ref([])
 
 
+const columns = [
+  { field: 'id', header: 'Concluir' },
+  { field: 'orderTodo', header: 'Ordem' },
+];
+
+
+
 function removeYear(value) {
   const [date, hours] = value.split(' ');
   const [day, month] = date.split('/');
   return `${hours} ${day}/${month}`;
 }
 
-const columns = [
-  { field: 'description', header: 'Descrição' },
-  { field: 'id', header: 'Concluir' },
-  { field: 'orderTodo', header: 'Ordem' },
-];
+
+const onCellEditComplete = (event) => {
+    let { data, newValue } = event
+    if(newValue != data.description) {
+      updateDescriptionById(data.id, newValue)
+    }
+};
+
+
 
 
 
@@ -83,6 +103,22 @@ async function changeOrderById(id) {
   const URL_ORDER_DATA = `${URL+id}/order`
 
   await fetch(URL_ORDER_DATA, {method: "PATCH"})
+
+  fetchData()
+}
+
+
+
+async function updateDescriptionById(id, description) {
+
+  const URL_UPDATE_DESCRIPTION = `${URL+id}/update-description`
+
+  await fetch(URL_UPDATE_DESCRIPTION,
+  {
+    method: "PATCH",
+    headers: HEADERS,
+    body: JSON.stringify({ 'description': description })
+  })
 
   fetchData()
 }
